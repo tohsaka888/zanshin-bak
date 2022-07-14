25765713b66f3adb1d3cee4eed49a1645d545319
tohsaka888 <156132264@qq.com>
2022-07-14 16:18:51 +0800

    feat: chakra-ui配置

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <DndProvider backend={HTML5Backend}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </DndProvider>
   )
 }
