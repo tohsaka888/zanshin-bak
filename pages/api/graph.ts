993bba566617357ab64dc11db301a711ada3cb44
tohsaka888 <156132264@qq.com>
2022-07-14 16:20:14 +0800

    api: 图接口

--- pages/api/graph.ts
+++ pages/api/graph.ts
@@ -1,13 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import { genGraphData } from '../../mock/graph_data'
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Api.GraphResponse>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ success: true, data: genGraphData() })
 }
